a1f1cf1b7591a7777f5b280cbb9d82e1fb387a45
Jonathan <jonathanthornton1997@gmail.com>
2020-06-25 14:28:49 -0700

    updated styles

--- app/src/Components/Home/Navigation.js
+++ app/src/Components/Home/Navigation.js
@@ -4,20 +4,22 @@ import {Link} from "react-router-dom";
 export default function Nav (props) {
 
     return (
-    <nav>
-          
-    <ul>
-      <li>
-        <Link to="/">Sign Up </Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      <li>
-        <Link to="/dashboard">Dashboard</Link>
-      </li>
-    </ul>
-  </nav>
+      <header className="navHeader">
+      <nav>
+            
+      <ul>
+        <li>
+          <Link className="link" to="/">Sign Up </Link>
+        </li>
+        <li>
+          <Link className="link" to="/login">Login</Link>
+        </li>
+        <li>
+          <Link className="link" to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    </nav>
+    </header>
     )
 
 }
